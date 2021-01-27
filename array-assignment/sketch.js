c8529fcc7165d945851286cc40cff3cc80852526
Amy Lu <yibingamy@gmail.com>
2021-01-26 21:56:46 -0600

    display grid

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -5,21 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [[0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
-            [0,0,0,0,0,0,0,0,0]
+let grid = [[0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0]];
 
 let rows, cols, cellWidth, cellHeight;
+let num;
+let selectCell;
+let cellX;
+let cellY;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background("white");
+  let myCanvas = createCanvas(windowHeight*0.8, windowHeight*0.8);
+  myCanvas.position(windowWidth*0.5 - (windowHeight*0.8 / 2), windowHeight*0.1);
+
+  rows = grid.length;
+  cols = grid[0].length;
+  cellWidth = width/cols;
+  cellHeight = height/rows;
+
 }
 
 function draw() {
@@ -30,14 +40,25 @@ function draw() {
 function drawGrid(){
   for (let y=0; y<rows; y++){
     for (let x=0; x<cols; x++){
-      if (grid[y][x] === 0){
-        fill("white");
-      }
-      else{
-        fill("black");
-      }
+      strokeWeight(2);
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      num = grid[y][x];
+      textSize(20);
+      textFont('VERDANA');
+      textAlign(CENTER, CENTER);
+      text(num, x*cellWidth + cellWidth/2, y*cellHeight + cellHeight/2);
+
     }
   }
 }
 
+function mousePressed(){
+  cellX = Math.floor(mouseX/cellWidth);
+  cellY = Math.floor(mouseY/cellHeight);
+
+  selectCell = true;
+}
+
+function keyIsPressed(){
+  if (selectCell === true);
+}
