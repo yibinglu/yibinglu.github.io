22a7dface99ab739e003b58e5579d4cd50ba14a8
Amy Lu <yibingamy@gmail.com>
2021-01-29 21:51:58 -0600

    new tester file

--- array-tester/sketch.js
+++ array-tester/sketch.js
@@ -10,44 +10,26 @@
 //Highlight row and box of selected number
 //Startscreen
 //Add music
-
-
-// let original = [[0, 0, 0, 2, 6, 0, 7, 0, 1],
-//                 [6, 8, 0, 0, 7, 0, 0, 9, 0],
-//                 [1, 9, 0, 0, 0, 4, 5, 0, 0],
-//                 [8, 2, 0, 1, 0, 0, 0, 4, 0],
-//                 [0, 0, 4, 6, 0, 2, 9, 0, 0],
-//                 [0, 5, 0, 0, 0, 3, 0, 2, 8],
-//                 [0, 0, 9, 3, 0, 0, 0, 7, 4],
-//                 [0, 4, 0, 0, 5, 0, 0, 3, 6],
-//                 [7, 0, 3, 0, 1, 8, 0, 0, 0]];
-
-// let answer = [[4, 3, 5, 2, 6, 9, 7, 8, 1],
-//               [6, 8, 2, 5, 7, 1, 4, 9, 3],
-//               [1, 9, 7, 8, 3, 4, 5, 6, 2],
-//               [8, 2, 6, 1, 9, 5, 3, 4, 7],
-//               [3, 7, 4, 6, 8, 2, 9, 1, 5],
-//               [9, 5, 1, 7, 4, 3, 6, 2, 8],
-//               [5, 1, 9, 3, 2, 6, 8, 7, 4],
-//               [2, 4, 8, 9, 5, 7, 1, 3, 6],
-//               [7, 6, 3, 4, 1, 8, 2, 5, 9]];
+//Number size ratio
 
 let rows, cols, cellWidth, cellHeight;
-let num;
+// let num;
 let selectCell = false;
 let x, y;
-let myCanvas;
+// let myCanvas;
 let click, complete;
 let answer;
 let playerGrid;
 let original;
 let mistakes = 0;
+let sidePadding, topPadding, gridSize;
+let sideEdge, vertEdge, bottomEdge;
 
 function preload(){
   click = loadSound("assets/click1.wav");
   complete = loadSound("assets/complete.mp3"); //doesn't do anything yet
   original = loadJSON("assets/sudoku1-original.json");
-  answer = loadJSON("assets/sudoku1-answer.json"); //used saveJSONArray() but still cannot get length
+  answer = loadJSON("assets/sudoku1-answer.json"); //tried saveJSONArray() but still cannot get length
   playerGrid = loadJSON("assets/sudoku1-player.json");
 }
 
@@ -61,15 +43,20 @@ function arrayLength(grid){
 }
 
 function setup() {
-  myCanvas = createCanvas(windowHeight*0.8, windowHeight*0.8);
-  myCanvas.position(windowWidth*0.5 - windowHeight*0.8 / 2, windowHeight*0.1);
+  createCanvas(windowWidth, windowHeight);
 
+  //center the grid
+  gridSize = windowWidth*0.38;
+  sidePadding = (windowWidth - gridSize)/2;
+  topPadding = (windowHeight - gridSize)/2;
+
+  sideEdge = sidePadding + gridSize;
+  vertEdge = topPadding + gridSize;
 
   rows = arrayLength(original);
   cols = original[0].length;
-  cellWidth = width/cols;
-  cellHeight = height/rows;
-
+  cellWidth = gridSize/cols;
+  cellHeight = gridSize/rows;
 }
 
 function draw() {
@@ -85,34 +72,37 @@ function drawGrid(){
       // if (selectCell){ only fill if x and y are equal to the saved position of selected cell
       //   fill(242, 239, 216);
       // }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      rect(x*cellWidth + sidePadding, y*cellHeight + topPadding, cellWidth, cellHeight);
       if (playerGrid[y][x] !== 0){
         textSize(25);
         textFont("VERDANA");
         textAlign(CENTER, CENTER);
-        text(playerGrid[y][x], x*cellWidth + cellWidth/2, y*cellHeight + cellHeight/2);
+        text(playerGrid[y][x], x*cellWidth + sidePadding + (cellWidth/2), y*cellHeight + topPadding + (cellHeight/2));
       }
-
     }
   }
+  drawGridOutline();
+}
+
+function drawGridOutline(){
   //draw border 
-  strokeWeight(5);
-  line(0, 0, myCanvas.width, 0); //top
-  line(myCanvas.width-1, myCanvas.width-1, 0, myCanvas.width-1); //bottom
-  line(myCanvas.width-1, 0, myCanvas.width-1, myCanvas.width-1); //right
-  line(0, 0, 0, myCanvas.width-1); //left
+  strokeWeight(2.5);
+  line(sidePadding, topPadding, sideEdge, topPadding); //top
+  line(sidePadding, topPadding + gridSize, sideEdge, vertEdge); //bottom
+  line(sideEdge, topPadding, sideEdge, vertEdge); //right
+  line(sidePadding, topPadding, sidePadding, vertEdge); //left
 
   //draw 3x3 outline
   strokeWeight(2.5);
-  line(0, myCanvas.width * 1/3, myCanvas.width-1, myCanvas.width * 1/3); // horiz. top
-  line(0, myCanvas.width * 2/3, myCanvas.width-1, myCanvas.width * 2/3); //horiz. bottom
-  line(myCanvas.width * 1/3, 0, myCanvas.width * 1/3, myCanvas.width-1); //vert. left
-  line(myCanvas.width * 2/3, 0, myCanvas.width * 2/3, myCanvas.width-1); //vert. right
+  line(sidePadding, topPadding + (gridSize * 1/3), sideEdge, topPadding + (gridSize * 1/3)); // horiz. top
+  line(sidePadding, topPadding + (gridSize * 2/3), sideEdge, topPadding + (gridSize * 2/3)); //horiz. bottom
+  line(sidePadding + (gridSize * 1/3), topPadding, sidePadding + (gridSize * 1/3), vertEdge); //vert. left
+  line(sidePadding + (gridSize * 2/3), topPadding, sidePadding + (gridSize * 2/3), vertEdge); //vert. right
 }
 
 function mousePressed(){
-  x = Math.floor(mouseX/cellWidth);
-  y = Math.floor(mouseY/cellHeight);
+  x = Math.floor((mouseX - sidePadding)/cellWidth);
+  y = Math.floor((mouseY - topPadding)/cellHeight);
 
   // save position of selected cell
   if (original[y][x] === 0) { //check if trying to select original number
@@ -144,8 +134,8 @@ function keyPressed(){
 
 function displayMistakes(){
   let mistakesText = "Mistakes: " + mistakes;
-  textSize(50);
+  textSize(20);
   textFont("VERDANA");
   textAlign(CENTER, CENTER);
-  text(mistakesText, 0, myCanvas.height + 10);
+  text(mistakesText, windowWidth - 100, 30);
 }
